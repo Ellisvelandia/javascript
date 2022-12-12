2ec883cdc0ded5740bb206a2c780f52e954518ff
ellisvelandia <eyis619@gmail.com>
2022-12-12 06:59:44 -0500

    loops

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
-// // let hours = prompt("enter the hours you want to spend on days")
+// // // // let hours = prompt("enter the hours you want to spend on days")
 
-// // alert(`${hours} is equal to ${hours/24}days`)
+// // // // alert(`${hours} is equal to ${hours/24}days`)
 
-// // conditionals
+// // // // conditionals
 
-// let name = prompt("enter your name");
+// // // let name = prompt("enter your name");
 
-// // if (name === "ellis") {
-// //   alert("your names is ellis");
-// // } else if (name === "cris") {
-// //   alert("your name is ellis is not cris");
-// // } else if (name !== "cris" || name !== "santos") {
-// //   alert("Your name is not cris and any is santos");
-// // } else {
-// //   alert("your names if no here on the list");
+// // // // if (name === "ellis") {
+// // // //   alert("your names is ellis");
+// // // // } else if (name === "cris") {
+// // // //   alert("your name is ellis is not cris");
+// // // // } else if (name !== "cris" || name !== "santos") {
+// // // //   alert("Your name is not cris and any is santos");
+// // // // } else {
+// // // //   alert("your names if no here on the list");
+// // // // }
+
+// // // // conditional switch case
+
+// // // switch (name) {
+// // //   case "ellis":
+// // //     alert("hello, welcome ellis");
+// // //     break;
+// // //   case "noellis":
+// // //     alert("is your name noellis");
+// // //     break;
+// // //   default:
+// // //     alert("your name is not ellis neither is it noellis");
+// // //     break;
+// // // }
+
+// // "use strict"
+
+// // let dates = new Array("date 1- one thing", "date 2", 123, true);
+
+// let fruits = ["banana", "strewberry", "grape"];
+
+// // fruits.push("peach")
+// // fruits.pop()
+// // alert(fruits)
+
+// // Loops
+// //while ,dowhile, for, for in, for of
+
+// let number = 0;
+
+// // while (number < 10) {
+// //   number ++;
+// //   document.write(number + '<br>');
+// // };
+
+// // do {
+// //   number++;
+// //   document.write(number + "<br>");
+// // } while (number < 10);
+
+// // for (let i = 0; i <= 10; i++) {
+// //   document.write(5 + " x " + i + " = " + 5 * i + "<br>");
 // // }
 
-// // conditional switch case
-
-// switch (name) {
-//   case "ellis":
-//     alert("hello, welcome ellis");
-//     break;
-//   case "noellis":
-//     alert("is your name noellis");
-//     break;
-//   default:
-//     alert("your name is not ellis neither is it noellis");
-//     break;
+// for (let i of fruits) {
+//   document.write(i + "<br>");
 // }
