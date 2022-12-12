b79697213dc3aa689ef6b4e71580c1704bcd9b30
ellisvelandia <eyis619@gmail.com>
2022-12-12 06:13:35 -0500

    ifelse

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-// let hours = prompt("enter the hours you want to spend on days")
+// // let hours = prompt("enter the hours you want to spend on days")
 
-// alert(`${hours} is equal to ${hours/24}days`)
+// // alert(`${hours} is equal to ${hours/24}days`)
 
-// conditionals
+// // conditionals
 
-let name = prompt("enter your name");
+// let name = prompt("enter your name");
 
-if (name === "ellis") {
-  alert("your names is ellis");
-} else if (name === "cris") {
-  alert("your name is ellis is not cris");
-} else if (name !== "cris" || name !== "santos") {
-  alert("Your name is not cris and any is santos");
-} else {
-  alert("your names if no here on the list");
-}
+// // if (name === "ellis") {
+// //   alert("your names is ellis");
+// // } else if (name === "cris") {
+// //   alert("your name is ellis is not cris");
+// // } else if (name !== "cris" || name !== "santos") {
+// //   alert("Your name is not cris and any is santos");
+// // } else {
+// //   alert("your names if no here on the list");
+// // }
+
+// // conditional switch case
+
+// switch (name) {
+//   case "ellis":
+//     alert("hello, welcome ellis");
+//     break;
+//   case "noellis":
+//     alert("is your name noellis");
+//     break;
+//   default:
+//     alert("your name is not ellis neither is it noellis");
+//     break;
+// }
