ea409fbc69cc9a8b92ae8b6ecda9eba3ee1dc8a1
ellisvelandia <eyis619@gmail.com>
2022-12-11 21:30:10 -0500

    type of dates javascript

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-const array =[1,2,3] [10] = 4
+let videogame = "callofduty";
 
-console.log(array.length)
+console.log(videogame);
+
+let num1 = "12";
+let num2 = 12;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+
+let boolean = true;
+
+console.log(boolean);
+
+let number1 = "disk";
+let number2 = 9;
+let number3 = undefined;
+
+let words1 = "pepsi";
+let words2 = "coca-cola";
+
+console.log(number1 + number2);
+console.log(number3 + number2);
+console.log(words1 + words2);
+
+// concat
+console.log("awesome" + words1 + "is better" + words2);
+
+// types of data
+// string "" ''
+// Number 0
+// Boolean true | false
+// NaN
+// undefined
+// null
+
+
+// template literal
+console.log(`how ${words1} while drinks ${words2}`);
