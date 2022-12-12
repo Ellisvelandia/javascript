0b5a22d79983e1aa89fbdb09d1fe899f2b2601ae
ellisvelandia <eyis619@gmail.com>
2022-12-12 09:25:43 -0500

    methodstring

---
+++ methods.js
@@ -0,0 +1,47 @@
+// // // //method parseInt = transforms to whole number
+// // // //method parsefloat = transforms to decimal number
+
+// // // let number1 = "10.92100";
+
+// // // let number2 = number1.toString(1)
+
+// // // // document.write(typeof number2);
+// // // document.write(number2);
+
+// // //isNaN
+// // //isInteger
+
+// // // let age = parseInt(prompt("what is your age?"));
+
+// // let age = "18";
+// // let age2 = Number.isInteger(age);
+
+// // document.write(age2);
+
+// // Methods string
+
+// // let a = "Hello world"
+// // document.write(a.length)
+
+// // let b = "hello ellis"
+// // document.write(a.indexOf("world"))
+
+// // let c = "Hello, how are you today, i hope you are well like me" + "<br>";
+
+// let a =
+//   "monday, tuesday, wednesday, thursday, thursday, friday, saturday, sunday";
+// // let b = a.split(", ");
+// // let d = "hope you are well like me";
+
+// // let e = c.match("are you");
+
+// // document.write(c.toUpperCase)
+// // document.write(c.substring(5, 10));
+// // document.write(c.charAt(15));
+// // document.write(c.endsWith("H"));
+// // document.write(c.includes("oda"));
+// // document.write(c.repeat(15));
+// // document.write(c.replace("how", "hows"));
+// // document.write(c.slice(8));
+// // document.write(b[0]);
+// document.write(a.trim());
